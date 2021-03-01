76ae9d2e41a0ba74c35cacaee0870fc5f2e619c0
hiboma <hito@pepabo.com>
2021-03-01 12:55:53 +0900

    first commit

---
+++ attributes/event_handler.rb
@@ -0,0 +1,2 @@
+default['event_handler']['git_repo'] = 'https://github.com/example/example'
+default['event_handler']['git_hash'] = 'undefined'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,34 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+require 'syslog'
+
+# Chef.event_handler 内では、node オブジェクトを呼ぶのがちょっと面倒なのでここで入れちゃう
+git_repo = node[:event_handler][:git_repo]
+git_hash = node[:event_handler][:git_hash]
+
+# イベントタイプ一覧はこちら https://docs.chef.io/handlers/#event-types
+Chef.event_handler do
+  on :converge_start do |run_context|
+    Syslog.open("chef-infra-client") do |syslog|
+      syslog.info("kafkacluster-chef deploy started repository:%s revision:%s", git_repo, git_hash)
+    end
+  end
+end
+
+Chef.event_handler do
+  on :converge_complete do
+    Syslog.open("chef-infra-client") do |syslog|
+      syslog.info("kafkacluster-chef deploy finished repository:%s revision:%s", git_repo, git_hash)
+    end
+  end
+end
+
+Chef.event_handler do
+  # Failのときも一応syslogへ入れて、一旦終了したことを記録します
+  on :converge_failed do
+    Syslog.open("chef-infra-client") do |syslog|
+      syslog.info("kafkacluster-chef deploy finished repository:%s revision:%s", git_repo, git_hash)
+    end
+  end
+end
