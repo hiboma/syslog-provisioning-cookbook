4490fd52049eeed11ed4a28247cf198c0deddc6b
hiboma <hito@pepabo.com>
2021-03-01 15:52:17 +0900

    introduce 'tag' attribute

--- attributes/event_handler.rb
+++
@@ -1,2 +0,0 @@
-default['syslog-provisioning']['git_repo'] = `git remote get-url --push origin`.chomp
-default['syslog-provisioning']['git_hash'] = `git log -1 --pretty=format:"%H"`.chomp
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,8 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 require 'syslog'
 
+tag      = node[:'syslog-provisioning'][:tag]
+
 # Chef.event_handler 内では、node オブジェクトを呼ぶのがちょっと面倒なのでここで入れちゃう
 git_repo = node[:'syslog-provisioning'][:git_repo]
 git_hash = node[:'syslog-provisioning'][:git_hash]
@@ -12,7 +14,7 @@ git_hash = node[:'syslog-provisioning'][:git_hash]
 # イベントタイプ一覧はこちら https://docs.chef.io/handlers/#event-types
 Chef.event_handler do
   on :converge_start do |run_context|
-    Syslog.open("chef-infra-client") do |syslog|
+    Syslog.open(tag) do |syslog|
       syslog.info("provisioning started repository:%s revision:%s", git_repo, git_hash)
     end
   end
@@ -20,7 +22,7 @@ end
 
 Chef.event_handler do
   on :converge_complete do
-    Syslog.open("chef-infra-client") do |syslog|
+    Syslog.open(tag) do |syslog|
       syslog.info("provisioning finished repository:%s revision:%s", git_repo, git_hash)
     end
   end
@@ -29,7 +31,7 @@ end
 Chef.event_handler do
   # Failのときも一応syslogへ入れて、一旦終了したことを記録します
   on :converge_failed do
-    Syslog.open("chef-infra-client") do |syslog|
+    Syslog.open(tag) do |syslog|
       syslog.info("provisioning finished repository:%s revision:%s", git_repo, git_hash)
     end
   end
