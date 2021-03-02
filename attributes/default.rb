01be7f50fb41b36bd2ed76c76512c1485da5ba46
hiboma <hito@pepabo.com>
2021-03-02 18:54:38 +0900

    fix: use 'chef-infra-client'

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['syslog-provisioning']['tag']      = 'chef-client'
+default['syslog-provisioning']['tag']      = 'chef-infra-client'
 default['syslog-provisioning']['git_repo'] = 'undefined'
 default['syslog-provisioning']['git_hash'] = 'undefined'
